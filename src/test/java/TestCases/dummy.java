542950abb18e21d6e8998d4d415ed82db653ad20
Deepak <Deepak.saini2106@gmail.com>
2025-02-08 00:07:53 +0530

    final changes

--- src/test/java/TestCases/dummy.java
+++ src/test/java/TestCases/dummy.java
@@ -12,16 +12,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class dummy   extends BaseTest {
+public class dummy extends BaseTest {
 
-	@Test(groups = { "regression" }, priority = 0, description = "Login with Exisitng User in Application", dataProvider="loginData",dataProviderClass = TestDataProvider.class)
+	@Test(groups = {
+			"regression" }, priority = 0, description = "Login with Exisitng User in Application", dataProvider = "loginData", dataProviderClass = TestDataProvider.class)
 	public void loginIntoApplication(String username, String password) {
-	
 
-
-
-				HomePage homePage = new HomePagePopUp(getDriver()).navigateToURL().verifyCookiesAndPopUp().clickOnAccount()
-						.enterEmailId("deepaksn987@gmail.com").enterPassword("Flipkart.com@21").clickOnSignIn();
+		HomePage homePage = new HomePagePopUp(getDriver()).navigateToURL().verifyCookiesAndPopUp().clickOnAccount()
+				.enterEmailId("deepaksn987@gmail.com").enterPassword("Flipkart.com@21").clickOnSignIn();
 	}
 
 }
