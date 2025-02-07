2c5784b41b6d5a69358810051af6da18167fa10e
Deepak <Deepak.saini2106@gmail.com>
2025-02-07 15:16:32 +0530

    hi 2025 jab

--- src/test/java/TestCases/TC_01_MyProtien_Login_using_BaseTest2.java
+++ src/test/java/TestCases/TC_01_MyProtien_Login_using_BaseTest2.java
@@ -30,7 +30,7 @@ public class TC_01_MyProtien_Login_using_BaseTest2 extends BaseTest {
 
 		HomePagePopUp homePage = new HomePagePopUp(getDriver()).navigateToURL();
 
-		log.info("Login test passed successfully.");
+		log.info("Login test passed successfully. :)");
 
 	}
 
