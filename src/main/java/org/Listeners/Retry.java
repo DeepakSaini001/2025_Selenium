73101e8dcf007716a62e0e040200784d47520c68
Deepak <Deepak.saini2106@gmail.com>
2025-02-13 13:34:14 +0530

    retry code changes

--- src/main/java/org/Listeners/Retry.java
+++ src/main/java/org/Listeners/Retry.java
@@ -2,27 +2,28 @@ package org.Listeners;
 
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
+import org.utils.CustomUtility;
 
-import com.learning.data.Constants;
-import com.learning.utilities.Custom_Utilties;
 
-public class Retry extends Custom_Utilties implements IRetryAnalyzer {
+public class Retry extends CustomUtility implements IRetryAnalyzer {
 
-	int retryCount = Constants.RetryCount;
-	int maxRetryCount = Constants.MaxRetryCount;
+	int retryCount = 0;
+	int maxRetryCount = 3;
 
 	public boolean retry(ITestResult result) {
 		if (retryCount < maxRetryCount) {
 
-			/*
-			 * System.out.println("Retrying test " + result.getName() + " with status " +
-			 * getResultStatusName(result.getStatus()) + " for the " + (retryCount + 1) +
-			 * " time(s).");
-			 */
+			
+			  System.out.println("Retrying test " + result.getName() + " with status " +
+			  getResultStatusName(result.getStatus()) + " for the " + (retryCount + 1) +
+			  " time(s).");
+			 
 
-			publishMessageInReports("Retrying test " + result.getName() + " with status "
-					+ getResultStatusName(result.getStatus()) + " for the " + (retryCount + 1) + " time(s).");
-
+				/*
+				 * publishMessageInReports("Retrying test " + result.getName() + " with status "
+				 * + getResultStatusName(result.getStatus()) + " for the " + (retryCount + 1) +
+				 * " time(s).");
+				 */
 			retryCount++;
 			return true;
 		}
